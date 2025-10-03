d0611b40106209cc63ea20663c7940d0cd7d365a
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2025-10-03 15:40:34 +0100

    Remove eslint auto save

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,12 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-
+  react: true,
+  typescript: false,
+  stylistic: {
+    indent: 2,
+  },
+  rules: {
+    'no-console': 'off',
+  },
 })
