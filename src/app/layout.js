72e64ecb77ccb51ea73bb7cf58f7c16e1df1cb49
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2025-10-03 15:01:13 +0100

    Remove extra space

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "SustainWear",
-  description: "SustainWear: A Smart Clothes Donation and Sustainability Platform ",
+  description: "SustainWear: A Smart Clothes Donation and Sustainability Platform",
 };
 
 export default function RootLayout({ children }) {
