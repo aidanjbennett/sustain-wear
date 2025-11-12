20a18537d3be95aba39074f84beb222cde1a25ab
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2025-11-12 15:06:04 +0000

    Correct spelling

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request) {
