2432579ac542fe0d9d5bfa9ae502edc5c8a8b7bc
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2025-11-15 09:57:34 +0000

    Add Navbar to all pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-
-export default function navbar() {
+export default function Navbar() {
   return (
-    
-<nav className="bg-green-700">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"> SustainWear</span>
-  <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" class="text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Login</button>
-  </div>
-  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-    <ul>
-        <a href='' className='text-xl font-semibold whitespace-nowrap dark:text-white py-2 px-7 hover:text-gray-200'>test link</a>
-    </ul>
-    <ul>
-        <a href='' className='text-xl font-semibold whitespace-nowrap dark:text-white py-2 px-7 hover:text-gray-200'>test link</a>
-    </ul>
-    <ul>
-        <a href='' className='text-xl font-semibold whitespace-nowrap dark:text-white py-2 px-7 hover:text-gray-200'>test link</a>
-    </ul>
-    <ul>
-        <a href='' className='text-xl font-semibold whitespace-nowrap dark:text-white py-2 px-7 hover:text-gray-200'>test link</a>
-    </ul>
-  </div>
-  </div>
-</nav>
+
+    <nav className="bg-green-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"> SustainWear</span>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button type="button" className="text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Login</button>
+        </div>
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+          <ul>
+            <a href='' className='text-xl font-semibold whitespace-nowrap dark:text-white py-2 px-7 hover:text-gray-200'>test link</a>
+          </ul>
+          <ul>
+            <a href='' className='text-xl font-semibold whitespace-nowrap dark:text-white py-2 px-7 hover:text-gray-200'>test link</a>
+          </ul>
+          <ul>
+            <a href='' className='text-xl font-semibold whitespace-nowrap dark:text-white py-2 px-7 hover:text-gray-200'>test link</a>
+          </ul>
+          <ul>
+            <a href='' className='text-xl font-semibold whitespace-nowrap dark:text-white py-2 px-7 hover:text-gray-200'>test link</a>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
   )
 }
