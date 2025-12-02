7d04e0afddc09461473ddd2f45db8463685717de
jhallam00 <158037841+jhallam00@users.noreply.github.com>
2025-12-02 17:55:37 +0000

    style pages

--- src/app/donor/donate/page.jsx
+++ src/app/donor/donate/page.jsx
@@ -15,49 +15,55 @@ export default async function Donate() {
   }
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <form action={addDonation}>
-        <label htmlFor="type">Type of clothing</label>
-        <input
+    <div className="h-screen flex items-center justify-center">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 w-xl max-w-xs"
+      action={addDonation}>
+        <p className="font-bold text-center text-2xl mb-4">Donate</p>
+        <label className="font-bold" htmlFor="type">Type of clothing</label>
+        <input 
           id="type"
           name="type"
           type="text"
           required
+          className="border rounded w-full py-2 px-3 mb-6 bg-gray-100"
         />
-
-        <label htmlFor="size">Size of clothing</label>
+        
+        <label className="font-bold" htmlFor="size">Size of clothing</label>
         <input
           id="size"
           name="size"
           type="text"
           required
+          className="border rounded w-full py-2 px-3 mb-6 bg-gray-100"
         />
 
-        <label htmlFor="brand">Brand of clothing</label>
-        <input
+        <label className="font-bold" htmlFor="brand">Brand of clothing</label>
+        <input 
           id="brand"
           name="brand"
           type="text"
           required
+          className="border rounded w-full py-2 px-3 mb-6 bg-gray-100"
         />
 
-        <label htmlFor="colour">Colour of clothing</label>
-        <input
+        <label className="font-bold" htmlFor="colour">Colour of clothing</label>
+        <input 
           id="colour"
           name="colour"
           type="text"
           required
+          className="border rounded w-full mb-6 bg-gray-100"
         />
 
-        <label htmlFor="condition">Condition of clothing</label>
-        <select name="condition" id="condition">
+        <label className="font-bold px-2 mb-4" htmlFor="condition">Condition:</label>
+        <select className="mb-4" name="condition" id="condition">
           <option value="fair">Fair</option>
           <option value="good">Good</option>
           <option value="excellent">Excellent</option>
         </select>
-        <button type="submit">
-          Add Item
+        <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 mb-6 rounded w-full"
+        type="submit">
+          Submit Donation
         </button>
       </form>
     </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,23 +5,23 @@ export default function Home() {
     <main>
       <h1 className="font-bold text-2xl text-center mt-2">Sustain Wear</h1>
       <p className="text-center text-xl">Connecting donors to charity&apos;s since 2024</p>
-      <div className="md:flex rounded shadow-md justify-between mx-40 gap-4 mt-10">
+      <div className="md:flex rounded shadow-md justify-between mx-40 gap-4 mt-10 bg-white">
         <div className="w-1/2">
-          <h2 className="text-center text-xl underline mb-2">For Donors</h2>
+          <h2 className="text-center text-2xl underline mb-2">For Donors</h2>
           <ul>
-            <li className="text-center">Easy to donate</li>
-            <li className="text-center">See history of donations</li>
-            <li className="text-center">See if donation is accepted</li>
-            <li className="text-center">Knowledge that you are helping</li>
+            <li className="text-center my-4 ">Easy to donate</li>
+            <li className="text-center my-4 ">See history of donations</li>
+            <li className="text-center my-4 ">See if donation is accepted</li>
+            <li className="text-center my-4 ">Knowledge that you are helping</li>
           </ul>
         </div>
         <div className="w-1/2">
-          <h2 className="text-center text-xl underline mb-2">For Charities</h2>
+          <h2 className="text-center text-2xl underline mb-2">For Charities</h2>
           <ul>
-            <li className="text-center">Access to more donors</li>
-            <li className="text-center">Ablity to sort through donations</li>
-            <li className="text-center">Amazing Community</li>
-            <li className="text-center">Ablity to see donation history</li>
+            <li className="text-center my-4 ">Access to more donors</li>
+            <li className="text-center my-4 ">Ablity to sort through donations</li>
+            <li className="text-center my-4 ">Amazing Community</li>
+            <li className="text-center my-4 ">Ablity to see donation history</li>
           </ul>
           <div className="flex justify-center mt-20 w-40">
       <button type="signup" className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded w-full">
